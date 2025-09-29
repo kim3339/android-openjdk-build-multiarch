7cd389e7f865b0e5d1b85a63a6e6259b7cee6a88
kim3339 <48321404+kim3339@users.noreply.github.com>
2025-09-29 19:52:35 +0900

    Update 6_buildjdk.sh

--- 6_buildjdk.sh
+++ 6_buildjdk.sh
@@ -86,7 +86,7 @@ ln -s -f $CUPS_DIR/cups $ANDROID_INCLUDE/
 cd openjdk-${TARGET_VERSION}
 
 # Apply patches
-git reset --hard ba6bbe5cf55ea7dddb074462312e314ad0d63484
+git reset --hard 01d107aea8eca4f4d2863deb30ac60e802debe15
 if [[ "$BUILD_IOS" != "1" ]]; then
   find ../patches/jre_${TARGET_VERSION}/android -name "*.diff" -print0 | xargs -0 -I {} sh -c 'echo "Applying {}" && git apply  --reject --whitespace=fix {} || (echo "git apply failed (Android patch set)" && exit 1)' 
 else
