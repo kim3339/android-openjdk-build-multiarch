bacbd1f4b9636684fb167909ad6c7edfb207777f
kim3339 <48321404+kim3339@users.noreply.github.com>
2025-09-29 19:48:29 +0900

    Update 5_clonejdk.sh

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -4,5 +4,5 @@ set -e
 if [[ $TARGET_VERSION -eq 21 ]]; then
     git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
 else
-    git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    git clone https://github.com/openjdk/jdk17u openjdk-17
 fi
